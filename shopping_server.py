2bd2cdf184cd5219ab227807b7a8643f3273e39b
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-03-23 10:19:23 -0500

    Closed files

--- shopping_server.py
+++ shopping_server.py
@@ -11,17 +11,13 @@ def index():
 def save():
     content = request.get_json()
     text = open("remotestorage.txt","w")
-    text.write(json.dumps(content))
+    data = text.write(json.dumps(content))
     text.close()
-
-    # with open("remotestorage.txt", "w") as f:
-        # f.write(json.dumps(content))
     return redirect(url_for("index"))
 
 @app.route("/getlist")
 def getList():
     text = open("remotestorage.txt","r+")
-    # with open("remotestorage.txt","r") as f:
     data = text.read()
     text.close()
     return data
